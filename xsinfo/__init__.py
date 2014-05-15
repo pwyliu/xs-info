bab80ff5e46d1ac89eaf07d45bf1466264750fa6
pliu <paul_liu@kanetix.ca>
2014-05-15 12:40:00 -0400

    app looks for a DB encryption key

--- xsinfo/__init__.py
+++ xsinfo/__init__.py
@@ -3,13 +3,15 @@ import os
 import sys
 
 
-def configure_app(app, conf_filename='conf.py', secret_filename='app.key'):
+def configure_app(app, load_ek=False, conf_filename='conf.py',
+                  secret_filename='app.key', ek_filename='encryption.key'):
     """
     http://flask.pocoo.org/snippets/104/
     """
 
     conf_filename = os.path.join(app.instance_path, conf_filename)
     secret_filename = os.path.join(app.instance_path, secret_filename)
+    ek_filename = os.path.join(app.instance_path, ek_filename)
 
     try:
         app.config.from_pyfile(conf_filename)
@@ -28,8 +30,16 @@ def configure_app(app, conf_filename='conf.py', secret_filename='app.key'):
         print 'head -c 64 /dev/urandom > {}\n'.format(secret_filename)
         sys.exit(1)
 
+    if load_ek:
+        try:
+            app.config['ENCRYPTION_KEY'] = open(ek_filename, 'rb').read()
+        except IOError:
+            print ('Error: No DB encryption key found. '
+                   'Deloy before starting application.')
+            sys.exit(1)
+
 # init app
 app = Flask(__name__, instance_relative_config=True)
-configure_app(app)
+configure_app(app, load_ek=True)
 
 import xsinfo.views
